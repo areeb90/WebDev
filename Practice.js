12c0aa62a7e0fd02fbef6614c50d2cd584eea4fb
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2021-03-04 01:22:26 -0800

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -32,8 +32,6 @@ count++;
 
 
 
-
-
 // listens keyboard press events 
 document.addEventListener("keypress", function(event){
     console.log(event);
@@ -66,4 +64,28 @@ function anotherAddeventlistener(typeOfEvent, Callback){
 // calling
 anotherAddeventlistener("keypress", function(event){
     console.log(event);
-})
+});
+
+
+
+
+
+
+
+
+// sett event listener on button element 
+
+// color change using JS 
+for (i = 0; i<5; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color= "purple";
+    });
+};
+
+// "button",  "h1" could be any element 
+  
+//  color change using JQuery 
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
